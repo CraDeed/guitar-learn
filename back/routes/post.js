9de32161ffb5bd66908fbe98be16735aae8a4778
CraDeed <56147655+CraDeed@users.noreply.github.com>
2021-07-17 12:48:30 +0900

    크롤링 문제 해결

--- back/routes/post.js
+++ back/routes/post.js
@@ -20,38 +20,13 @@ router.post('/search', async (req, res, next) => {
     const { artist, music } = req.query;
 
     const result = spawn('python', ['back/python/craw.py', artist, music]);
-    // const result = spawn('python', ['back/python/ex.py', '카레유', '20']);
-
-    //TODO: undefined2 고치기
-
-    // // 3. stdout의 'data'이벤트리스너로 실행결과를 받는다.
-    // result.stdout.on('data', function (data) {
-    //   const textChunk = data.toString();
-    //   try {
-    //     // console.log(textChunk);
-    //     // console.log('type', typeof textChunk);
-    //     const obj = JSON.parse(textChunk);
-    //     // console.log(obj);
-    //     res.status(200).send(obj);
-    //   } catch (error) {
-    //     console.log(error);
-    //     res.status(201).send({});
-    //   }
-    // });
 
     let obj;
 
+    // 3. stdout의 'data'이벤트리스너로 실행결과를 받는다.
     result.stdout.on('data', (data) => {
-      // var buff2 = Buffer.from('hellow word');
-      // console.log(buff2);
-      // console.log(buff2.toString());
-      // console.log(data);
-      // console.log(data.toString());
-      // console.log(JSON.parse(data.toString()));
-      // console.log(data);
-      const dataToSend = data.toString();
-      console.log(dataToSend);
-      obj = JSON.parse(dataToSend);
+      const textChunk = data.toString();
+      obj = JSON.parse(textChunk);
     });
 
     result.on('close', (code) => {
